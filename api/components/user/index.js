4341b02f1dbc8b442f19819bda0be0e69ec4acdc
juancodev <montillasanchezjuancarlos@gmail.com>
2024-01-05 01:24:46 -0400

    [⚡ADD] serverless with vercel

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,13 @@
 // const store = require('./../../../store/mysql');
-const store = require('../../../store/remote-mysql');
+const config = require('../../../config');
+
+let store;
+if (config.remoteDB === true) {
+  store = require('../../../store/remote-mysql');
+} else {
+  store = require('./../../../store/mysql');
+}
+
 const controller = require('./controller');
 
 module.exports = controller(store);
